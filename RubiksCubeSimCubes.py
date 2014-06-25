463e7b54626d750f1ba33a87d849e16ede9926e3
codeprogress <codeprogress1@gmail.com>
2014-06-24 23:56:10 -0400

    Update RubiksCubeSimCubes.py

---
+++ RubiksCubeSimCubes.py
@@ -0,0 +1,21 @@
+# Rubik's Cube Simulation, as cube objects
+
+# create the 26 cube objects
+
+threeSides = [['g','o','w'], ['g','w','r'],['g','r','y'],['g','y','o'],
+              ['b','o','w'], ['b','w','r'],['b','r','y'],['b','y','o']]
+
+twoSides   = [['g','o'], ['g','w'], ['g','r'], ['g','y'],
+              ['b','o'], ['b','w'], ['b','r'], ['b','y'],
+              ['w','o'], ['o','y'], ['y','r'], ['r','w']]
+
+oneSide    = [['g'], ['o'], ['w'], ['r'], ['b'], ['y']]
+
+#Right Face
+#Left Face
+#Up Face
+#Down Face
+#Front Face
+#Back Face
+
+#translation of 16 moves if looking at a different face
